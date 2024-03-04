b2f6882e610df6b00f1be8c0252076c2a8e99337
KalebSand <157537090+KalebSand@users.noreply.github.com>
2024-03-03 21:11:37 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "movielist.h"
+#include <fstream>
+#include<iostream>
+using namespace std;
+
+int main() {
+    ifstream inFile;
+    inFile.open("movies.txt");
+
+    if (!inFile.is_open())
+        cout << "File: movies.txt is not found" << endl;
+
+    MovieList movieListSystem;
+
+    movieListSystem.runMovieSystem();
+
+    inFile.close();
+
+    return 0;
+}
---
+++ movieList.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#ifndef MOVIELIST_H
+#define MOVIELIST_H
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+const int INITIAL_CAPACITY = 30;
+const int MAX_REVIEW_LENGTH = 25;
+const string FILE_PATH = "movies.txt";
+
+struct Movie {
+    string name;
+    int rating;
+    double releaseYear;
+    string review;
+};
+
+class MovieList {
+public:
+    MovieList();
+    ~MovieList();
+
+    void runMovieSystem();
+    void addMovie();
+    void removeMovie();
+    void modifyMovie();
+    void displayMovieList();
+    void displayMoviesInRange();
+    void searchMovie();
+    void bubbleSort();
+    void resizeArrays();
+    void saveDataToFile();
+    void loadDataFromFile();
+
+private:
+    Movie* movies;
+    int capacity;
+    int pos;
+};
+
+#endif // MOVIELIST_H
